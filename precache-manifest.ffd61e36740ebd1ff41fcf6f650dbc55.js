cb301d037d6fdc327585f1c8da3efac15651f311
Oded Nir <odednir@icloud.com>
2020-09-14 12:55:16 +0300

    Updates

---
+++ precache-manifest.ffd61e36740ebd1ff41fcf6f650dbc55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff42f8582b4aaf3458a6d797b075e81f",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "74d8cb3470cf08cbe1d0",
+    "url": "/my-website/static/css/main.94647594.chunk.css"
+  },
+  {
+    "revision": "8df8112e69b59de09749",
+    "url": "/my-website/static/js/2.ae648cbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-website/static/js/2.ae648cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d8cb3470cf08cbe1d0",
+    "url": "/my-website/static/js/main.0f0f6ba4.chunk.js"
+  },
+  {
+    "revision": "8a31e1a8153659a682c8",
+    "url": "/my-website/static/js/runtime-main.0cbcd75a.js"
+  },
+  {
+    "revision": "f88ddb6a4d97e62bbc7e51b0788c98f8",
+    "url": "/my-website/static/media/Oded Nir - Resume.f88ddb6a.pdf"
+  },
+  {
+    "revision": "aea0ca06abe6895995257cae536e972d",
+    "url": "/my-website/static/media/profile-picture.aea0ca06.jpg"
+  }
+]);
